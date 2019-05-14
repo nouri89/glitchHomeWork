74bfb584a3fbcd9a5e2f9daf6c4a6a19ba9b473b
Glitch (hello-express) <none>
2019-05-14 15:38:18 +0000

    🍧🌟 Checkpoint ./server.js:3234086/1502

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-// server.js
-// where your node app starts
-
-// init project
-const express = require('express');
+//Load the 'express' library which makes it easier 
+//to handle HTTP conversations:
+const express = require("express");
 
 const app = express();
 
-app.get('/', function(request, response) {
-  response.send("hello CYF")
+//Register some handlers for different routes.
+//Read about routing here:
+//http://expressjs.com/en/starter/basic-routing.html
+//You can add more routes.
+
+app.get("/", function (request, response) {
+    response.send("Hello CYF");
 });
 
-
-app.get('/data', function(request, response) {
-  response.json([1,2,3])
+app.get("/two", function (request, response) {
+    response.send("Another route");
 });
 
-// listen for requests :)
+app.get("/numbers", function(request, response) {
+  response.json([1, 2, 3]);
+});
+
+//Tell the server to start listening for requests.
+//This will prevent the program from exiting.
+//It will keep running, and when a matching request 
+//comes in, it will call the handlers we registered above.
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-
-
-//read about routing
-// http://expressjs.com/en/starter/basic-routing.html
