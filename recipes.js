0c7bfc8bdbf93aca74d825b88d60308bd48efc2a
Glitch (hello-express) <none>
2019-05-31 16:36:35 +0000

    🚐🍽 Checkpoint ./server.js:3234086/4357 ./recipes.js:3234086/418

--- recipes.js
+++ recipes.js
@@ -1,4 +1,6 @@
-exports.valididateRecipe = (recipe) => {
+
+
+function valididateRecipe(recipe) {
   if (!recipe ||
       !recipe.title ||
       !recipe.ingredients ||
@@ -7,8 +9,21 @@ exports.valididateRecipe = (recipe) => {
       !recipe.author ||
       !recipe.countryOfOrigin ||
       !recipe.course) {
-    console.log(' bad ', { recipe })
+    console.log('Recipe failed validation: ', { recipe })
     return false
   }
   return true
 }
+
+
+function updateRecipeInPlace(orig, changes) {
+  orig.title = changes.title;
+  orig.ingredients = changes.ingredients;
+  orig.prepTime = changes.prepTime;
+  orig.steps = changes.steps;
+  orig.author = changes.author;
+  orig.countryOfOrigin = changes.countryOfOrigin;
+  orig.course = changes.course;
+}
+
+exports = { updateRecipeInPlace, valididateRecipe};
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
-let recipes = require("./recipes.json");
-const { valididateRecipe } = require("./recipes.js");
+const recipes = require("./recipes.json");
+const { valididateRecipe, updateRecipeInPlace } = require("./recipes.js");
 
 const app = express();
 
@@ -11,6 +11,10 @@ app.use(cors());
 
 app.get("/", function(request, response) {
   response.send("Welcome to CYF recipes server");
+});
+
+app.get("/recipes", function(request, response) {
+  response.json(recipes);
 });
 
 app.get("/recipes/:id", function(request, response) {
@@ -23,33 +27,16 @@ app.get("/recipes/:id", function(request, response) {
   }
 });
 
-app.get("/recipes", function(request, response) {
-    response.json(recipes);
-});
-
-function updateRecipeInPlace(orig, changes){
-  orig.title = changes.title;
-  orig.ingredients = changes.ingredients;
-  orig.prepTime = changes.prepTime;
-  orig.steps = changes.steps;
-  orig.author = changes.author;
-  orig.countryOfOrigin = changes.countryOfOrigin;
-  orig.course = changes.course;
-}
-
 app.post("/recipes", function(request, response) {
   const recipe = request.body;
-  // if (!valididateRecipe(recipe)) {
-  //   return response.sendStatus(400)
-  // }
   recipe.id = recipes.length + 1;
   recipes.push(recipe);
   response.status(201).json(recipe);
 });
 
 app.put("/recipes/:id", function(request, response) {
-  const id = request.params.id
-  
+  const id = request.params.id;
+
   const recipeSubmitted = request.body;
 
   const existingRecipe = recipes.find(r => id == r.id);
@@ -58,18 +45,18 @@ app.put("/recipes/:id", function(request, response) {
     response.json(existingRecipe);
   } else {
     response.sendStatus(404);
-  }  
+  }
 });
 
 app.delete("/recipes/:id", function(request, response) {
   const id = request.params.id;
 
   const indexToDelete = recipes.findIndex(item => item.id == id);
-  if (indexToDelete >= 0){
+  if (indexToDelete >= 0) {
     recipes.splice(indexToDelete, 1);
     response.sendStatus(204);
   } else {
-    response.sendStatus(404);    
+    response.sendStatus(404);
   }
 });
 
