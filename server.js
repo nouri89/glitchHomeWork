9850b5c1bceabbe680048bd53ae9dbb1b3f37668
Glitch (hello-express) <none>
2019-05-31 16:06:15 +0000

    🍜🍧 Checkpoint ./server.js:3234086/3412

--- server.js
+++ server.js
@@ -1,48 +1,54 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 
-let recipes = require('./recipes.json')
-const { valididateRecipe } = require("./recipes.js")
+let recipes = require("./recipes.json");
+const { valididateRecipe } = require("./recipes.js");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get("/", function (request, response) {
-    response.send("Welcome to CYF recipes server");
+app.get("/", function(request, response) {
+  response.send("Welcome to CYF recipes server");
 });
 
-app.get("/recipes/:id?", function(request, response) {
+app.get("/recipes/:id", function(request, response) {
   const id = request.params.id;
-  if (!id) return response.json(recipes)
-  
-  const recipe = recipes.find(r => id == r.id)
-  if (recipe === undefined) return response.sendStatus(404)
-  response.json(recipes);
+
+  const recipe = recipes.find(r => id == r.id);
+  if (recipe) {
+    response.json(recipe);
+  } else {
+    response.sendStatus(404);
+  }
 });
 
-
-
-app.post("/recipes", function (request, response) {
-    const recipe = request.body
-    // if (!valididateRecipe(recipe)) {
-    //   return response.sendStatus(400)
-    // }
-    recipe.id = recipes.length+1
-    recipes.push(recipe)
-    response.status(201).json(recipe)
+app.get("/recipes", function(request, response) {
+    response.json(recipes);
 });
 
-app.delete("/recipes/:id", function (request, response) {
+app.post("/recipes", function(request, response) {
+  const recipe = request.body;
+  // if (!valididateRecipe(recipe)) {
+  //   return response.sendStatus(400)
+  // }
+  recipe.id = recipes.length + 1;
+  recipes.push(recipe);
+  response.status(201).json(recipe);
+});
+
+app.delete("/recipes/:id", function(request, response) {
   const id = request.params.id;
-  if (id === undefined) return response.sendStatus(400)
-  
-  const recipe = recipes.find(item => item.id == id)
-  if (recipe === undefined) return response.sendStatus(404)
+  if (id === undefined) return response.sendStatus(400);
 
-  recipes = recipes.filter((item) => { return item.id != id })
-  response.sendStatus(204)
-})
+  const recipe = recipes.find(item => item.id == id);
+  if (recipe === undefined) return response.sendStatus(404);
+
+  recipes = recipes.filter(item => {
+    return item.id != id;
+  });
+  response.sendStatus(204);
+});
 
 app.listen(process.env.PORT);
